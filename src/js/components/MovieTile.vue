8d00977b9b503378efdf452c4d09dbf3a009c38c
Valerii Osadchuk <v.osadchuk@astoundcommerce.com>
2023-05-31 21:59:47 +0300

    vue: simple component to render movies

---
+++ src/js/components/MovieTile.vue
@@ -0,0 +1,36 @@
+<template>
+  <article class="movie-tile">
+    <picture class="movie-tile__picture">
+      <img :src="movie.poster" :alt="movie.title" />
+    </picture>
+    <div class="movie-tile__info">
+      <h2 class="movie-tile__title">{{ movie.title }}</h2>
+      <p class="movie-tile__description">{{ movie.overview }}</p>
+    </div>
+  </article>
+</template>
+
+<script lang="ts">
+export default {
+  props: ["movie"],
+};
+</script>
+
+<style lang="scss">
+.movie-tile {
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+
+  &__picture {
+    width: 8rem;
+    flex-shrink: 0;
+    font-size: 0;
+
+    img {
+      width: 100%;
+      height: auto;
+    }
+  }
+}
+</style>
