59389e3ba380a9d676ed3fdd53ac800a5f91a86c
Valerii Osadchuk <v.osadchuk@astoundcommerce.com>
2023-06-07 16:48:30 +0300

    vue: loader indicator for movie poster

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,16 @@ import { createApp } from "vue";
 import './style.scss';
 
 import App from "./App.vue";
+import AppPage from './js/ui/AppPage.vue';
+import AppLoader from './js/ui/AppLoader.vue'
 import AppSection from './js/ui/AppSection.vue';
 import AppSectionHeader from './js/ui/AppSectionHeader.vue';
-import AppPage from './js/ui/AppPage.vue';
 
 const app = createApp(App);
 
+app.component("AppPage", AppPage);
+app.component("AppLoader", AppLoader);
 app.component("AppSection", AppSection);
 app.component("AppSectionHeader", AppSectionHeader);
-app.component("AppPage", AppPage);
 
 app.mount('#app');
