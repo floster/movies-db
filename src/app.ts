41da1e7c634eb63cdbd456a8deca8a4e370ff344
Valerii Osadchuk <v.osadchuk@astoundcommerce.com>
2023-07-20 19:50:35 +0300

    project: alert component

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,33 @@
-console.log('Hello World');
+const favoriteTogglers = document.querySelectorAll('.favorite-toggle');
+const appAlert = document.querySelector('.app-alert') as HTMLElement;
+const appAlertCloseBtn = document.querySelector('.app-alert__close') as HTMLElement;
+
+/**
+ * Opens the app alert
+ * @param {boolean} delayedClose - true if need to close the alert after a delay of 3 seconds.
+ * @returns {void}
+ */
+function appAlertOpen(delayedClose = false) {
+    appAlert.classList.remove('is-close');
+    appAlert.classList.add('is-open');
+
+    if (delayedClose) {
+        setTimeout(() => {
+            appAlertClose();
+        }, 3000);
+    }
+}
+
+function appAlertClose() {
+    appAlert.classList.remove('is-open');
+    appAlert.classList.add('is-close');
+}
+
+appAlertCloseBtn.addEventListener('click', appAlertClose);
+
+favoriteTogglers.forEach((toggler) => {
+    toggler.addEventListener('click', _ => {
+        appAlertOpen(true);
+    });
+});
+
