4a863dc92c0479ccbde7e4875b2028062d93423a
Valerii Osadchuk <v.osadchuk@astoundcommerce.com>
2023-07-23 13:45:43 +0300

    project: using handlebars.js as template language

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import handlebars from '@vituum/vite-plugin-handlebars'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    handlebars({
+      partials: {
+        directory: resolve(__dirname, 'src', 'partials'),
+        extname: false
+      },
+      data: resolve(__dirname, 'src', 'data.json'),
+      helpers: {
+        removeDot: (num: number) => num.toString().replace('.', '')
+      }
+    })
+  ],
+  build: {
+    rollupOptions: {
+      input: ['index.hbs']
+    }
+  }
 })
