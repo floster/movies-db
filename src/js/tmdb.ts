91bbcaf7c3f0ce39f82d9ed0d3019b6efda3d601
Valerii Osadchuk <v.osadchuk@astoundcommerce.com>
2023-07-24 13:21:12 +0300

    react: init react-swc

--- src/js/tmdb.ts
+++ src/js/tmdb.ts
@@ -14,20 +14,14 @@ import {
   ListData,
 } from './types';
 
-import { store } from './store';
-
 class TMDB {
   async #getJSON(url: string): Promise<any> {
-    try {
-      const response: Response = await fetch(url);
+    const response: Response = await fetch(url);
 
-      if (!response.ok) throw new Error(`getJSON: Error fetching data for URL: ${url}`);
+    if (!response.ok) throw new Error(`getJSON: Error fetching data for URL: ${url}`);
 
-      const data: any = await response.json();
-      return data;
-    } catch (error) {
-      throw error;
-    }
+    const data: any = await response.json();
+    return data;
   }
 
   #normalizeMovie(movie: RawMovie): Movie {
@@ -107,17 +101,13 @@ class TMDB {
   }
 
   async getMovie(id: number): Promise<Movie> {
-    try {
-      const url = `${API_BASE}/movie/${id}${API_KEY}`;
-      const data = await this.#getJSON(url);
-      console.log(data);
+    const url = `${API_BASE}/movie/${id}${API_KEY}`;
+    const data = await this.#getJSON(url);
+    console.log(data);
 
-      const movie = this.#normalizeMovie(data);
+    const movie = this.#normalizeMovie(data);
 
-      return movie;
-    } catch (error) {
-      throw error;
-    }
+    return movie;
   }
 
   // async #discoverMovies(page = 1): Promise<MovieListRespose> {
@@ -137,18 +127,18 @@ class TMDB {
   //     const movies = await this.#discoverMovies(page);
   //     movies.results.forEach(movie => ids.add(movie.id));
   //   }
-    
+
   //   return Array.from(ids);
   // }
 
-  async getRandomMovie(): Promise<Movie> {
-    const ids = store.top500;
-    const randomIdx = Math.floor(Math.random() * ids.length) + 1;
-    const randomID = ids[randomIdx - 1];
-    const movie = await this.getMovie(randomID);
+  // async getRandomMovie(): Promise<Movie> {
+  //   const ids = store.top500;
+  //   const randomIdx = Math.floor(Math.random() * ids.length) + 1;
+  //   const randomID = ids[randomIdx - 1];
+  //   const movie = await this.getMovie(randomID);
 
-    return movie;
-  }
+  //   return movie;
+  // }
 }
 
 export default new TMDB();
