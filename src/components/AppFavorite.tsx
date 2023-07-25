fe6921599b6d4ad6f77e12f7fe16d3d283520011
Valerii Osadchuk <v.osadchuk@astoundcommerce.com>
2023-07-25 21:11:44 +0300

    react: remove alert show when favorite state changed

--- src/components/AppFavorite.tsx
+++ src/components/AppFavorite.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import AppAlert from "./AppAlert";
 import SvgIcon from "./SvgIcon";
 
 interface AppFavoriteProps {
@@ -8,23 +7,16 @@ interface AppFavoriteProps {
 }
 
 export const AppFavorite = ({ checked, title }: AppFavoriteProps) => {
-    const [alertVisible, setAlertVisible] = useState(false);
     const [checkedState, setCheckedState] = useState(checked);
     const toggleCheckedState = () => setCheckedState(!checkedState);
 
-    const showAlert = () => setAlertVisible(true);
-    const hideAlert = () => setAlertVisible(false);
-
-    const favoriteAction = checkedState ? 'added to' : 'removed from';
-
     return (
         <>
-            <label className="app-favorite" aria-label={`add ${title} to favorites`} onClick={showAlert}>
+            <label className="app-favorite" aria-label={`add ${title} to favorites`}>
                 <input type="checkbox" name="toggleFavoriteCheckbox" id="" defaultChecked={checkedState} onChange={toggleCheckedState} />
                 <SvgIcon icon="fire" extraClass="m-outline" />
                 <SvgIcon icon="fire_solid" extraClass="m-solid" />
             </label>
-            <AppAlert isOpened={alertVisible} onClose={hideAlert} text={title} action={favoriteAction} />
         </>
     );
 };
