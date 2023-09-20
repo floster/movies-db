c6519b99d35c66850bc4f7b01358b6dfbb3adc58
mr.valerio <valera.osadchuk@gmail.com>
2023-09-20 11:33:55 +0300

    project: move default sort to .env

--- src/hooks/useSortOption.ts
+++ src/hooks/useSortOption.ts
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { USortOptionValues } from "../types/tmdb.types";
 
+const defaultSortOption = import.meta.env.VITE_DEFAULT_SORT_OPTION as USortOptionValues;
+
 export const useSortOption = () => {
-    const [currentSortOption, setCurrentSortOption] = useState('year_desc' as USortOptionValues);
+    const [currentSortOption, setCurrentSortOption] = useState(defaultSortOption as USortOptionValues);
 
     const onSortChange = (option: string) => setCurrentSortOption(option as USortOptionValues);
 
