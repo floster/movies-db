9321da60f1cb0bc66b8c92232d810c65353accb8
mr.valerio <valera.osadchuk@gmail.com>
2023-09-20 16:40:50 +0300

    react: added useTilesPagination to 'collection' and 'movie' pages

--- src/pages/collection.tsx
+++ src/pages/collection.tsx
@@ -4,7 +4,6 @@ import AppSectionHeader from '../components/AppSectionHeader';
 import MediaHero from '../components/MediaHero';
 import AppTile from '../components/AppTile';
 import { useParams } from 'react-router-dom';
-import { ITileData } from '../types/tmdb.types';
 import AppSpinner from '../components/AppSpinner';
 import tmdb from '../js/tmdb-api';
 import AppError from '../components/AppError';
@@ -13,6 +12,7 @@ import { formatTilesData } from '../js/formatters';
 
 import { useTilesSort } from '../hooks/useTilesSort';
 import { useSortOption } from '../hooks/useSortOption';
+import { useTilesPagination } from './../hooks/useTilesPagination'
 
 type CollectionParams = {
   id: string;
@@ -22,21 +22,27 @@ export default function Collection() {
   const params = useParams<CollectionParams>();
   const collectionId = getIdFromLink(params.id!);
 
-  const [tiles, setTiles] = useState([] as ITileData[]);
-
   const [isDataLoading, setIsDataLoading] = useState(false);
   const [isDataError, setIsDataError] = useState(false);
 
   const collectionSortOption = useSortOption();
 
-  const { sortedTiles } = useTilesSort(tiles, collectionSortOption.currentSortOption);
+  const {
+    quantity,
+    currentPage,
+    currentTiles,
+    handleShowMore,
+    initPagination
+  } = useTilesPagination();
+
+  const { sortedTiles } = useTilesSort(currentTiles, collectionSortOption.currentSortOption);
 
   const getPartsData = useCallback(async () => {
     try {
       setIsDataLoading(true);
       const data = await tmdb.getCollection(collectionId);
       const formattedTiles = formatTilesData(data.parts, 'movie', 'year', true, true);
-      setTiles(formattedTiles);
+      initPagination(formattedTiles);
     } catch (error) {
       setIsDataError(true);
       console.error(error);
@@ -64,7 +70,16 @@ export default function Collection() {
               <div className="l-tiles_grid m-movies">
                 {isDataLoading
                   ? <AppSpinner visible={true} />
-                  : sortedTiles.map((tile) => <AppTile tile={tile} key={tile.id} />)
+                  : <>
+                    {sortedTiles.map((tile) => <AppTile tile={tile} key={tile.id} />)}
+                    <button
+                      className="app-button"
+                      onClick={() => handleShowMore()}
+                      disabled={currentPage >= quantity.pages}
+                    >
+                      show more ({currentPage} / {quantity.pages})
+                    </button>
+                  </>
                 }
               </div>
             </AppSection>
