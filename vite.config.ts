531c1d1b14edf2fd6660a620aee595e7d4e7da6a
mr.valerio <valera.osadchuk@gmail.com>
2023-09-25 23:11:57 +0300

    project: get rid of handlebars tails

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,7 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
-import handlebars from '@vituum/vite-plugin-handlebars'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    handlebars({
-      partials: {
-        directory: resolve(__dirname, 'src', 'partials'),
-        extname: false
-      },
-      data: resolve(__dirname, 'src', 'data.json'),
-      helpers: {
-        removeDot: (num: number) => num.toString().replace('.', '')
-      }
-    })
-  ],
-})
+  plugins: [react()],
+});
