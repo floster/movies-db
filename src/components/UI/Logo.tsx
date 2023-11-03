598ce0b953af8dc7dc9569be0b1bedbc2781bf3f
mr.valerio <valera.osadchuk@gmail.com>
2023-11-03 11:43:17 +0200

    PROJ:ADD: mobile version for Site Logo

--- src/components/UI/Logo.tsx
+++ src/components/UI/Logo.tsx
@@ -1,8 +1,19 @@
-import SvgIcon from './SvgIcon'
-
 const Logo: React.FC = () => (
   <a className="logo flex" href="/" aria-label="go to homepage">
-    <SvgIcon icon="logo_light" extraClass="logo__svg" logo={true} />
+    <picture className="logo__picture">
+      <source
+        srcSet="./public/mdb-logo.svg"
+        type="image/svg+xml"
+        media="(min-width: 744px)"
+      />
+      <img
+        className="logo__img"
+        src="./public/mdb-logo-sm.svg"
+        alt="Movies DB logo"
+        width="721"
+        height="98"
+      />
+    </picture>
   </a>
 )
 
