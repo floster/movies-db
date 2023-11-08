5ae02e823ab6441796db422227d27ee7bf270050
mr.valerio <valera.osadchuk@gmail.com>
2023-11-08 15:45:08 +0200

    REACT:ADD: remove unused code related to favorites state in account slice

--- src/store/slices/account.slice.ts
+++ src/store/slices/account.slice.ts
@@ -1,18 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { ISupabaseAccountMeta } from '../../types/supabase.models'
-import { IFavoritesState } from '../../types/tmdb.models'
-import { INITIAL_FAVORITES_STATE } from '../../config'
 
 type AccountState = {
   isAuthorized: boolean
   account: ISupabaseAccountMeta | null
-  favorites: IFavoritesState
 }
 
 const initialState: AccountState = {
   isAuthorized: false,
   account: null,
-  favorites: INITIAL_FAVORITES_STATE,
 }
 
 export const accountSlice = createSlice({
@@ -24,9 +20,6 @@ export const accountSlice = createSlice({
     },
     setAccount: (state, { payload }) => {
       state.account = payload
-    },
-    setFavorites: (state, { payload }) => {
-      state.favorites = payload
     },
   },
 })
