ccdf2efe3d2e8779b65583c382bcdd42f61fe6ec
mr.valerio <valera.osadchuk@gmail.com>
2023-11-08 14:21:15 +0200

    SUPABASE:ADD: fetch user favorites from Supabase and store them in store

--- src/hooks/supabase/getUserData.ts
+++ src/hooks/supabase/getUserData.ts
@@ -1,14 +1,31 @@
+const PROFILES_TABLE = import.meta.env.VITE_SB_PROFILES_TABLE
+const FAVS_COLUMN = import.meta.env.VITE_SB_FAVS_COLUMN
+
 import useGetSession from './getSession'
 import { useAppActions } from '../useRedux'
 import { useEffect } from 'react'
+import { supabase } from '../../supabase/client'
 
 const useGetUserData = () => {
-  const { setAuthorized, setAccount } = useAppActions()
+  const { setAuthorized, setAccount, setFavorites } = useAppActions()
   const { user, error } = useGetSession()
 
   useEffect(() => {
     setAuthorized(!!user)
-    if (!error && user) setAccount(user)
+    if (!error && user) {
+      setAccount(user)
+
+      supabase
+        .from(PROFILES_TABLE as string)
+        .select(FAVS_COLUMN)
+        .then(({ data, error }) => {
+          if (error) {
+            console.error(error)
+            return
+          }
+          setFavorites(JSON.parse(data[0][FAVS_COLUMN] ?? data[1][FAVS_COLUMN]))
+        })
+    }
   }, [user])
 }
 
