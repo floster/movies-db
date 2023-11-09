d4cab2d1483d031d78006d1ca0d912c093e33cda
mr.valerio <valera.osadchuk@gmail.com>
2023-11-09 13:22:33 +0200

    SUPABASE:FIX: refactor getUserData hook

--- src/hooks/supabase/getUserData.ts
+++ src/hooks/supabase/getUserData.ts
@@ -1,5 +1,4 @@
 const PROFILES_TABLE = import.meta.env.VITE_SB_PROFILES_TABLE
-const FAVS_COLUMN = import.meta.env.VITE_SB_FAVS_COLUMN
 
 import useGetSession from './getSession'
 import { useAppActions } from '../useRedux'
@@ -7,8 +6,12 @@ import { useEffect } from 'react'
 import { supabase } from '../../supabase/client'
 import { INITIAL_FAVORITES_STATE } from '../../config'
 
+import { PostgrestSingleResponse } from '@supabase/postgrest-js'
+import { IRawSupabaseProfile } from '../../types/supabase.models'
+
 const useGetUserData = () => {
-  const { setAuthorized, setAccount, setInitialFavorites } = useAppActions()
+  const { setAuthorized, setGod, setAccount, setInitialFavorites } =
+    useAppActions()
   const { user, error } = useGetSession()
 
   useEffect(() => {
@@ -18,22 +21,28 @@ const useGetUserData = () => {
 
       supabase
         .from(PROFILES_TABLE as string)
-        .select(FAVS_COLUMN)
-        .then(({ data, error }) => {
-          if (error) {
-            console.error(error)
-            return
+        .select('*')
+        .then(
+          ({ data, error }: PostgrestSingleResponse<IRawSupabaseProfile[]>) => {
+            console.log('* data', data)
+
+            if (error) {
+              console.error(error)
+              return
+            }
+
+            setGod(data[0].god_mode)
+
+            const _fetchedFavorites =
+              data && data[0] && data[0].favorites
+                ? JSON.parse(data[0].favorites)
+                : data && data[1] && data[1].favorites
+                ? JSON.parse(data[1].favorites)
+                : INITIAL_FAVORITES_STATE
+
+            setInitialFavorites(_fetchedFavorites)
           }
-
-          const _fetchedFavorites =
-            data && data[0] && data[0][FAVS_COLUMN]
-              ? JSON.parse(data[0][FAVS_COLUMN])
-              : data && data[1] && data[1][FAVS_COLUMN]
-              ? JSON.parse(data[1][FAVS_COLUMN])
-              : INITIAL_FAVORITES_STATE
-
-          setInitialFavorites(_fetchedFavorites)
-        })
+        )
     }
   }, [user])
 }
--- src/types/supabase.models.ts
+++ src/types/supabase.models.ts
@@ -12,6 +12,17 @@ export interface IRawSupabaseUserMeta {
   user_name?: string
 }
 
+export interface IRawSupabaseProfile {
+  id: string
+  updated_at: Date
+  username: string
+  full_name: string
+  avatar_url: string
+  favorites: string
+  locale: string
+  god_mode: boolean
+}
+
 export interface ISupabaseAccountMeta {
   id: string
   avatar_url: string
