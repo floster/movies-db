81a161ba7a9abee7d97b4a8f823628accb6ac143
mr.valerio <valera.osadchuk@gmail.com>
2023-12-11 11:49:43 +0200

    nuxt + nuxtui added

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="grid place-content-center min-h-screen gap-8">
+    <UButton
+      icon="i-heroicons-pencil-square"
+      size="xl"
+      color="sky"
+      variant="soft"
+      label="Button"
+      :trailing="false"
+    />
+
+    <p class="font-mono text-rose-600">monospace text</p>
+  </div>
+</template>
